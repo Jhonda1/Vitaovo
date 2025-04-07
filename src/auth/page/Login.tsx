d09881a790d350857aceb6d522ca481198fdec87
Jhon Dario Ortiz <jhon.ortiz@prosof.com.co>
2025-04-07 08:08:15 -0500

    se cambia el logo del inicio

--- src/auth/page/Login.tsx
+++ src/auth/page/Login.tsx
@@ -1,5 +1,5 @@
 import { FormEnterprise } from "../components/FormEnterprise";
-import LogoVitaovo from '../../assets/Logo.png';
+import LogoVitaovo from '../../assets/Prosof.png';
 import { useAuthStore } from "@/store/useAuthStore";
 import { FormUser } from "../components/FormUser";
 export const Login = () => {
@@ -7,8 +7,8 @@ export const Login = () => {
   return (
     <div className="w-full h-full flex items-center justify-center ">
       <div className="grid grid-cols-1 md:grid-cols-3 w-full h-full ">
-        <div className="bg-gradient-to-tl from-[#dcf6b5] via-[#92BD4E] to-[#d9e5c6] col-span-1 hidden md:flex items-center justify-center">
-          <img src={LogoVitaovo} className="object-cover h-[10%] lg:h-[15%]"/>
+        <div className="bg-gradient-to-tl from-[#dcf6b5]  to-[#d9e5c6] col-span-1 hidden md:flex items-center justify-center">
+          <img src={LogoVitaovo} className="object-cover h-[10%] lg:h-[10%]"/>
         </div>
         <div className="col-span-3 md:col-span-2 flex items-center justify-center  mx-4">
           {
